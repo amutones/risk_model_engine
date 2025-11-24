d1f8eeb862bd72aa6e98bb025d6564ae547ceb8e
amutones <aurgena@gmail.com>
2025-11-24 10:35:29 -0600

    Add data loader for portfolios

---
+++ risk-model/data-loader.py
@@ -0,0 +1,8 @@
+import pandas as pd
+from os.path import exists
+
+def load_portfolio(path: str) -> pd.DataFrame:
+    return null
+
+def load_factor_returns(path: str) -> pd.DataFrame:
+    return null
